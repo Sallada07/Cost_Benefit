6d97d95f0e624fb5e9c9d2727008ad61756d6193
Sallada <72992751+Sallada07@users.noreply.github.com>
2026-02-08 00:29:30 -0300

    initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+"""
+This is the main module for the application. It serves as the entry point for the program and is responsible for initializing the application. 
+
+The ideia is to get the perfomece ranking of differents cellphone models from the website "https://www.antutu.com/web/pt/ranking" and get the bests prices for each model from the weebsite "https://www.tudocelular.com/".
+
+In the future, I plan to add a cost-benefit graphic comparing the performance gain by the price increase.
+
+"""
+
